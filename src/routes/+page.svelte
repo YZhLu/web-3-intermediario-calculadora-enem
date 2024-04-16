a01aa1311f6d3ab676b3cd725ad26e4605ad8ae5
mkzin22 <marcospaulobento2006@gmail.com>
2024-04-16 20:31:12 -0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,63 @@
 <p>Esta calculadora serve para você entender que se tivesse aprendido matemática não teria tirado essa nota veia 💩</p>
 
 Insira sua tabela aki...
+    <table >
+        <tr>
+          <th>Prova do Enem</th>
+          <th>Nota mínima</th>
+          <th>Sua nota</th>
+          <th>Peso</th>
+          <th>Sua nota com peso</th>
+        </tr>
+        <tr>
+          <td>Redação</td>
+          <td>0,01</td>
+          <td>780,00</td>
+          <td>1,50</td>
+          <td>1.170,00</td>
+        </tr>
+        <tr>
+          <td>Linguagens, Códigos e suas tecnologias</td>
+          <td>0,01</td>
+          <td>552,10</td>
+          <td>1,00</td>
+          <td>552,10</td>
+        </tr>
+        <tr>
+          <td>Matemática e suas tecnologias</td>
+          <td>0,01</td>
+          <td>637,90</td>
+          <td>3,00</td>
+          <td>1.913,70</td>
+        </tr>
+        <tr>
+            <td>Ciências da Natureza e suas tecnologias</td>
+            <td>0,01</td>
+            <td>600,20</td>
+            <td>2,00</td>
+            <td>1.200,40</td>
+        </tr>
+        <tr>
+          <td>Ciências Humanas e suas tecnologias</td>
+          <td>0,01</td>
+          <td>581,90</td>
+          <td>1,00</td>
+          <td>581,90</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>Total</td>
+          <td>Total</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>8,50(A)</td>
+          <td>5.418,10(B)</td>
+        </tr>
+        <td><i>Nota do estudante (A+B) = 637,42</i></td>
+      </table>
